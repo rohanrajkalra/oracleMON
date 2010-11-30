ac2dcd5c21064db37a9f79d05f0e45111861dca0
Vicente Malaga <vmalaga@gmail.com>
2010-12-01 00:27:57 +0100

    Funciones de la barra de estado

--- ventPrinc.py
+++ ventPrinc.py
@@ -6,7 +6,7 @@ import wx.grid
 def create(parent):
     return ventPrincFrame(parent)
 
-[wxID_VENTPRINCFRAME, wxID_VENTPRINCFRAMENOTUSEDINDEXBOTON, 
+[wxID_VENTPRINCFRAME, wxID_VENTPRINCFRAMESTATICBITMAP1, 
  wxID_VENTPRINCFRAMESTATUSBAR1, wxID_VENTPRINCFRAMETOOLBAR1, 
 ] = [wx.NewId() for _init_ctrls in range(4)]
 
@@ -16,25 +16,11 @@ def create(parent):
 ] = [wx.NewId() for _init_coll_toolBar1_Tools in range(3)]
 
 class ventPrincFrame(wx.Frame):
-    def _init_sizers(self):
-        # generated method, don't edit
-        self.gridBagSizer1 = wx.GridBagSizer(hgap=0, vgap=0)
-
-        self._init_coll_gridBagSizer1_Items(self.gridBagSizer1)
-
-        self.SetSizer(self.gridBagSizer1)
-
-
-    def _init_coll_gridBagSizer1_Items(self, parent):
-        # generated method, don't edit
-
-        parent.AddWindow(self.NotUsedindexBoton, (0, 0), border=0, flag=0,
-              span=(1, 1))
+    
 
     def _init_coll_toolBar1_Tools(self, parent):
         # generated method, don't edit
 
-        #parent.DoAddTool(bitmap=wx.Bitmap(u'/home/vmalaga/Python_proyects/oracleMON/knetattach.png',
         parent.DoAddTool(bitmap=wx.Bitmap(u'img/knetattach.png',
               wx.BITMAP_TYPE_PNG), bmpDisabled=wx.NullBitmap,
               id=wxID_VENTPRINCFRAMETOOLBAR1CONECTAR_ID, kind=wx.ITEM_NORMAL,
@@ -58,13 +44,23 @@ class ventPrincFrame(wx.Frame):
 
         parent.Realize()
 
+    def _init_coll_statusBar1_Fields(self, parent):
+        # generated method, don't edit
+        parent.SetFieldsCount(3)
+
+        parent.SetStatusText(number=0, text=u'Desconectado')
+        parent.SetStatusText(number=1, text=u'Destino')
+        parent.SetStatusText(number=2, text=u'')
+
+        parent.SetStatusWidths([-3, -1, 30])
+
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_VENTPRINCFRAME, name=u'ventPrincFrame',
               parent=prnt, pos=wx.Point(416, 206), size=wx.Size(960, 533),
               style=wx.DEFAULT_FRAME_STYLE, title=u'oracleMON')
         self.SetClientSize(wx.Size(960, 533))
-        self.SetToolTipString(u'ventPrincFrame')
+        self.SetToolTipString(u'oracleMON')
         self.Center(wx.BOTH)
 
         self.toolBar1 = wx.ToolBar(id=wxID_VENTPRINCFRAMETOOLBAR1,
@@ -73,23 +69,21 @@ class ventPrincFrame(wx.Frame):
         self.SetToolBar(self.toolBar1)
 
         self.statusBar1 = wx.StatusBar(id=wxID_VENTPRINCFRAMESTATUSBAR1,
-              name='statusBar1', parent=self, style=0)
-        self.statusBar1.SetStatusText(u'desconectado')
+              name='statusBar1', parent=self, style=1)
+        self.statusBar1.SetStatusText(u'Desconectado')
         self.statusBar1.SetThemeEnabled(False)
+        self.statusBar1.SetLabel(u'')
+        self.statusBar1.SetHelpText(u'')
+        self.statusBar1.SetToolTipString(u'Estado de la conexion')
+        self._init_coll_statusBar1_Fields(self.statusBar1)
         self.SetStatusBar(self.statusBar1)
 
-        self.NotUsedindexBoton = wx.Button(id=wxID_VENTPRINCFRAMENOTUSEDINDEXBOTON,
-              label=u'Not Used Index', name=u'NotUsedindexBoton', parent=self,
-              pos=wx.Point(0, 0), size=wx.Size(100, 27), style=0)
-        self.NotUsedindexBoton.SetAutoLayout(False)
-        self.NotUsedindexBoton.SetToolTipString(u'Not used index')
-        self.NotUsedindexBoton.Bind(wx.EVT_BUTTON,
-              self.OnNotUsedindexBotonButton,
-              id=wxID_VENTPRINCFRAMENOTUSEDINDEXBOTON)
+        self.staticBitmap1 = wx.StaticBitmap(bitmap=wx.Bitmap(u'img/red.png',
+              wx.BITMAP_TYPE_PNG), id=wxID_VENTPRINCFRAMESTATICBITMAP1,
+              name='staticBitmap1', parent=self.statusBar1, pos=wx.Point(936,
+              8), size=wx.Size(24, 24), style=0)
 
         self._init_coll_toolBar1_Tools(self.toolBar1)
-
-        self._init_sizers()
 
     def __init__(self, parent):
         self._init_ctrls(parent)
@@ -121,6 +115,11 @@ class ventPrincFrame(wx.Frame):
                 print "Conectado a oracle"
                 global conectado
                 conectado = "TRUE"
+                constring = user+"@"+sid
+                self.statusBar1.SetStatusText("Conectado",0)
+                self.statusBar1.SetStatusText(constring,1)
+                self.staticBitmap1.SetBitmap(u'imp/green.png')
+                
             except cx_Oracle.DatabaseError, exc:
                 error, = exc.args
                 oraerror = "ORA-" + str(error.code)
